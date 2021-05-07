8a216bf93fa198059f652aed8762f026a555d078
malaba6 <malabadev6@gmail.com>
2021-05-07 14:51:33 +0300

    Add endpoint to mine the coin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
+import { v4 } from 'uuid';
 import Blockchain from '../model/blockchain';
+
 var express = require('express');
 var router = express.Router();
+const chain = new Blockchain();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +13,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/blockchain', (req, res, next) => {
-  const chain = new Blockchain();
   res.send({
     status: 'OK',
     chain: chain
@@ -18,11 +20,32 @@ router.get('/blockchain', (req, res, next) => {
 });
 
 router.get('/mine', (req, res, next) => {
-  res.send(' Get Proof of Work');
+  const nodeAddress = v4().split('-')?.join(' ');
+  chain.createNewTransaction(12.5, '00', nodeAddress);
+  
+  const lastBlock = chain.getLastBock();
+  const previousBlockHash = chain.getLastBock().hash;
+
+  const currentBlockData = {
+    transactions: chain.pendingTransactions,
+    index: lastBlock.index + 1
+  }
+  const nonce = chain.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = chain.hashBlock(previousBlockHash, currentBlockData, nonce);
+  const newBlock = chain.createNewBlock(nonce, previousBlockHash, blockHash);
+  
+  res.status(200).send({
+    status: 'New block mined successfully',
+    block: newBlock
+  })
 });
 
 router.post('/transaction', (req, res, next) => {
-  // res.send('View all the blockchain');
+  const blockIndex = chain.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.status(201).send({
+    status: 'Success',
+    message: `Transaction will be added in block ${blockIndex}`
+  })
 });
 
 
